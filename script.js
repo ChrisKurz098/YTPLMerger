6641e0dd7d85b5026f14388cf0a86fb0f999a0b7
CKURZ <ckurz098@gmail.com>
2021-12-17 12:50:03 -0800

    Init

--- script.js
+++ script.js
@@ -45,30 +45,10 @@ function mergePlaylist() {
   console.log(playlistData.result.items[0].contentDetails.videoId);
   for (let i = 0; i < 10; i++) {
 
-   let vidId =  playlistData.result.items[i].contentDetails.videoId;
-   
-   console.log("String VidId: ", vidId);
+    let vidId = playlistData.result.items[i].contentDetails.videoId;
 
-    /**
-* Sample JavaScript code for youtube.playlistItems.insert
-* See instructions for running APIs Explorer code samples locally:
-* https://developers.google.com/explorer-help/guides/code_samples#javascript
-*/
-
-    function authenticate() {
-      return gapi.auth2.getAuthInstance()
-        .signIn({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" })
-        .then(function () { console.log("Sign-in successful"); },
-          function (err) { console.error("Error signing in", err); });
-    }
-    function loadClient() {
-      gapi.client.setApiKey("AIzaSyCZuVb4_kTsIKbDBDGMM-KDRnIHyUoJyvw");
-      return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function () { console.log("GAPI client loaded for API"); },
-          function (err) { console.error("Error loading GAPI client for API", err); });
-    }
+    console.log("String VidId: ", vidId);
     // Make sure the client is loaded and sign-in is complete before calling this method.
-    function execute() {
       return gapi.client.youtube.playlistItems.insert({
         "part": [
           "snippet"
@@ -79,7 +59,7 @@ function mergePlaylist() {
             "position": 0,
             "resourceId": {
               "kind": "youtube#video",
-              "videoId": "'" + vidId + "'"
+              "videoId": vidId
             }
           }
         }
@@ -89,10 +69,8 @@ function mergePlaylist() {
           console.log("Response", response);
         },
           function (err) { console.error("Execute error", err); });
-    }
-    gapi.load("client:auth2", function () {
-      gapi.auth2.init({ client_id: "790008202741-acjnj5f0ssjnq29pbkm47qmkhmtq04ht.apps.googleusercontent.com" });
-    });
+    
+
 
   }
 }
