863bae7e356d2cd7884d0c2d670e6e8db04bf5bd
CKURZ <ckurz098@gmail.com>
2021-12-17 17:31:46 -0800

    added inputs for custom query

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function mergePlaylist() {
   console.log(playlistData.result.items[0].contentDetails.videoId);
   if (timer < max) {
     let vidId = playlistData.result.items[timer].contentDetails.videoId;
-if (vidId == undefined || !vidId){timer++;
+if (vidId === undefined || !vidId || !playlistData.result.items[timer].contentDetails){timer++;
   mergePlaylist();}
 
     console.log("String VidId: ", vidId);
